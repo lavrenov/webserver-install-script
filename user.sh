73ac42ddfb120561096e2cc63c66fc8ee20adef4
EvgenyLavr <lion06@inbox.ru>
2020-02-27 16:30:23 +0300

    Fix

--- user.sh
+++ user.sh
@@ -11,16 +11,16 @@ then
     if [[ "$action" == "add" ]];
     then
         useradd "$username" -p "$password" -d "$homeDir" -m -s /bin/bash
-	uid=$(id -u "$username")
-	ugid=$(id -g "$username")
-	echo $password | ftpasswd --stdin --passwd --file=/etc/proftpd/ftpd.passwd --name="$username" --uid="$uid" --gid="$ugid" --home="/var/www/$username" --shell=/bin/false
+	    uid=$(id -u "$username")
+	    ugid=$(id -g "$username")
+	    echo $password | ftpasswd --stdin --passwd --file=/etc/proftpd/ftpd.passwd --name="$username" --uid="$uid" --gid="$ugid" --home="/var/www/$username" --shell=/bin/false
     fi
 
     # Remove user
     if [[ "$action" == "remove" ]];
     then
         userdel -r "$username"
-	ftpasswd --passwd --file=/etc/proftpd/ftpd.passwd --name="$username" --delete-user
+	    ftpasswd --passwd --file=/etc/proftpd/ftpd.passwd --name="$username" --delete-user
     fi
 else
     echo "Required parameters not entered"
