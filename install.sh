a82b3d3a745d6b8703a27a919889d9642364c70b
EvgenyLavr <lion06@inbox.ru>
2020-02-28 12:23:16 +0300

    Update default php to 7.4

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ cp ./config/etc/apache2/apache2.conf /etc/apache2/apache2.conf 2>> "$log"
 cp ./config/etc/apache2/mods-available/remoteip.conf /etc/apache2/mods-available/remoteip.conf 2>> "$log"
 a2dismod mpm_event 2>> "$log"
 a2enmod mpm_prefork 2>> "$log"
-a2enmod php7.2 setenvif actions fcgid alias proxy_fcgi remoteip 2>> "$log"
+a2enmod php7.4 setenvif actions fcgid alias proxy_fcgi remoteip 2>> "$log"
 chown -R www-data:www-data /var/www 2>> "$log"
 rm -f /var/www/html/index.html
 rm -f /var/www/html/index.nginx-debian.html
@@ -37,10 +37,8 @@ echo "Install MariaDB" | tee -a "$log"
 apt install mariadb-server -y 2>> "$log"
 
 echo "Install PHP" | tee -a "$log"
-apt install php7.2 php7.2-fpm libapache2-mod-php7.2 php7.2-mysql php7.2-mysqli php7.2-curl php7.2-json php7.2-cgi php7.2-gd php7.2-zip php7.2-mbstring php7.2-xml php7.2-xmlrpc -y 2>> "$log"
-cp ./config/etc/php/7.2/apache2/php.ini /etc/php/7.2/apache2/php.ini 2>> "$log"
-cp ./config/etc/php/7.2/fpm/php.ini /etc/php/7.2/fpm/php.ini 2>> "$log"
-systemctl restart php7.2-fpm 2>> "$log"
+apt install php7.4 php7.4-fpm libapache2-mod-php7.4 php7.4-mysql php7.4-mysqli php7.4-curl php7.4-json php7.4-cgi php7.4-gd php7.4-zip php7.4-mbstring php7.4-xml php7.4-xmlrpc -y 2>> "$log"
+systemctl restart php7.4-fpm 2>> "$log"
 
 echo "Install Memcached" | tee -a "$log"
 apt install memcached php-memcached -y 2>> "$log"
