ceb3c9f7bd18fb8f2ba015b769724d80cd931045
EvgenyLavr <lion06@inbox.ru>
2020-03-02 12:20:13 +0300

    Add option separate fpm pool to site config

--- site.sh
+++ site.sh
@@ -3,40 +3,54 @@
 action=$1
 site=$2
 username=$3
+sepPool=$4
 nginxSiteDir="/etc/nginx/sites-enabled/"
 apacheSiteDir="/etc/apache2/sites-enabled/"
-wwwDir="/var/www/$username/"
+phpFpmPoolConf="/etc/php/7.4/fpm/pool.d/${username}_${site}.conf"
+wwwDir="/var/www/${username}/"
 
-if [[ -n "$site" ]] && [[ -n "$action" ]] && [[ -n "$username" ]];
+if [[ -n "${site}" ]] && [[ -n "${action}" ]] && [[ -n "${username}" ]];
 then
     # Add site
-    if [[ "$action" == "add" ]];
+    if [[ "${action}" == "add" ]];
     then
-        cp -i ./config/template/nginx.http.conf "$nginxSiteDir$site.conf"
-        sed -i "s/%DOMAIN%/$site/g" "$nginxSiteDir$site.conf"
-	sed -i "s/%USERNAME%/$username/g" "$nginxSiteDir$site.conf"
+        cp -i ./config/template/nginx.http.conf "${nginxSiteDir}${site}.conf"
+        sed -i "s/%DOMAIN%/${site}/g" "${nginxSiteDir}${site}.conf"
+        sed -i "s/%USERNAME%/${username}/g" "${nginxSiteDir}${site}.conf"
 
-        cp -i ./config/template/apache.host.conf "$apacheSiteDir$site.conf"
-        sed -i "s/%DOMAIN%/$site/g" "$apacheSiteDir$site.conf"
-	sed -i "s/%USERNAME%/$username/g" "$apacheSiteDir$site.conf"
+        cp -i ./config/template/apache.host.conf "${apacheSiteDir}${site}.conf"
+        sed -i "s/%DOMAIN%/${site}/g" "${apacheSiteDir}${site}.conf"
+        sed -i "s/%USERNAME%/${username}/g" "${apacheSiteDir}${site}.conf"
 
-        mkdir -p "$wwwDir$site/www"
-        mkdir -p "$wwwDir$site/tmp"
-        mkdir -p "$wwwDir$site/log/nginx"
-        mkdir -p "$wwwDir$site/log/apache"
+        mkdir -p "${wwwDir}${site}/www"
+        mkdir -p "${wwwDir}${site}/tmp"
+        mkdir -p "${wwwDir}${site}/log/nginx"
+        mkdir -p "${wwwDir}${site}/log/apache"
 
-        echo "<?php echo \"<h1>$site</h1>\"; ?>" > "$wwwDir$site/www/index.php"
+        echo "<?php echo \"<h1>${site}</h1>\"; ?>" > "${wwwDir}${site}/www/index.php"
 
-        chown -R "$username":"$username" "$wwwDir$site"
-        chmod -R 775 "$wwwDir$site"
+        chown -R "${username}":"${username}" "${wwwDir}${site}"
+        chmod -R 775 "${wwwDir}${site}"
+
+        if [[ -n "${sepPool}" ]];
+        then
+            cp -i ./config/template/php7.4-fpm.conf "${phpFpmPoolConf}"
+            sed -i "s/%POOLNAME%/${username}_${site}/g" "${phpFpmPoolConf}"
+            sed -i "s/%USERNAME%/${username}/g" "${phpFpmPoolConf}"
+            sed -i "s/%SITENAME%/_${site}/g" "${phpFpmPoolConf}"
+            systemctl restart php7.4-fpm
+            sed -i "s/%SITENAME%/_${site}/g" "${apacheSiteDir}${site}.conf"
+        else
+            sed -i "s/%SITENAME%//g" "${apacheSiteDir}${site}.conf"
+        fi
     fi
 
     # Remove site
-    if [[ "$action" == "remove" ]];
+    if [[ "${action}" == "remove" ]];
     then
-        rm -f "$nginxSiteDir$site.conf"
-        rm -f "$apacheSiteDir$site.conf"
-        rm -Rf "$wwwDir$site/"
+        rm -f "${nginxSiteDir}${site}.conf"
+        rm -f "${apacheSiteDir}${site}.conf"
+        rm -Rf "${wwwDir}${site}/"
     fi
 
     systemctl reload nginx
--- user.sh
+++ user.sh
@@ -3,31 +3,34 @@
 action=$1
 username=$2
 password=$3
-homeDir="/var/www/$username"
+homeDir="/var/www/${username}"
+phpFpmPoolConf="/etc/php/7.4/fpm/pool.d/${username}.conf"
 
-if [[ -n "$username" ]] && [[ -n "$action" ]];
+if [[ -n "${username}" ]] && [[ -n "${action}" ]];
 then
     # Add user
-    if [[ "$action" == "add" ]];
+    if [[ "${action}" == "add" ]];
     then
-        useradd "$username" -p "$password" -d "$homeDir" -m -s /bin/bash
-	uid=$(id -u "$username")
-	ugid=$(id -g "$username")
-	echo $password | ftpasswd --stdin --passwd --file=/etc/proftpd/ftpd.passwd --name="$username" --uid="$uid" --gid="$ugid" --home="/var/www/$username" --shell=/bin/false
-	mkdir -p "$homeDir/php"
-	cp ./config/template/php7.4-fpm.conf "/etc/php/7.4/fpm/pool.d/$username.conf"
-	sed -i "s/%USERNAME%/$username/g" "/etc/php/7.4/fpm/pool.d/$username.conf"
-	systemctl restart php7.4-fpm
+        useradd "${username}" -p "${password}" -d "${homeDir}" -m -s /bin/bash
+        uid=$(id -u "${username}")
+        ugid=$(id -g "${username}")
+        echo ${password} | ftpasswd --stdin --passwd --file=/etc/proftpd/ftpd.passwd --name="${username}" --uid="${uid}" --gid="${ugid}" --home="/var/www/${username}" --shell=/bin/false
+        mkdir -p "${homeDir}/php"
+        cp ./config/template/php7.4-fpm.conf "${phpFpmPoolConf}"
+        sed -i "s/%POOLNAME%/${username}/g" "${phpFpmPoolConf}"
+        sed -i "s/%USERNAME%/${username}/g" "${phpFpmPoolConf}"
+        sed -i "s/%SITENAME%//g" "${phpFpmPoolConf}"
+        systemctl restart php7.4-fpm
     fi
 
     # Remove user
-    if [[ "$action" == "remove" ]];
+    if [[ "${action}" == "remove" ]];
     then
-	systemctl stop php7.4-fpm
-        userdel -r "$username"
-	ftpasswd --passwd --file=/etc/proftpd/ftpd.passwd --name="$username" --delete-user
-	rm -f "/etc/php/7.4/fpm/pool.d/$username.conf"
-	systemctl start php7.4-fpm
+	    systemctl stop php7.4-fpm
+        userdel -r "${username}"
+	    ftpasswd --passwd --file=/etc/proftpd/ftpd.passwd --name="${username}" --delete-user
+	    rm -f "/etc/php/7.4/fpm/pool.d/${username}.conf"
+	    systemctl start php7.4-fpm
     fi
 else
     echo "Required parameters not entered"
