f6506a9ec7dbbd3c6fad4e49509a7e44b919346f
EvgenyLavr <lion06@inbox.ru>
2020-03-10 20:19:47 +0300

    Add backup script

---
+++ backup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERNAME=$1
+DB_USER=
+DB_PASS=
+DATE=`date +%Y-%m-%d_%H-%M-%S`
+BACKUP_DIR=/var/www/backups/${USERNAME}
+DATABASE_LIST=/tmp/databases.list
+WWW_DIR=/var/www
+
+mkdir -p ${BACKUP_DIR}/${USERNAME}/sql
+cp -ar ${WWW_DIR}/${USERNAME}/. ${BACKUP_DIR}/${USERNAME}
+
+mysql -u ${DB_USER} -p${DB_PASS} -e "show databases;" | grep "${USERNAME}_*" > ${DATABASE_LIST}
+
+for database in `cat ${DATABASE_LIST}`
+do
+    mysqldump -u ${DB_USER} -p${DB_PASS} ${database} > ${BACKUP_DIR}/${USERNAME}/sql/${database}.sql
+done
+
+chown -R ${USERNAME}:${USERNAME} ${BACKUP_DIR}
+rm -Rf ${BACKUP_DIR}/${USERNAME}/php
+cd ${BACKUP_DIR}
+tar -zcf ${DATE}.tar.gz ${USERNAME}
+rm -Rf ${USERNAME}
+rm -f ${DATABASE_LIST}
+chown ${USERNAME}:${USERNAME} ${DATE}.tar.gz
