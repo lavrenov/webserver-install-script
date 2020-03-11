b5193a6b36b8779abd1f6b6ebfe594bcd59505f9
EvgenyLavr <lion06@inbox.ru>
2020-03-11 16:33:52 +0300

    Add backup script apache, nginx, php-fpm configs

--- backup.sh
+++ backup.sh
@@ -8,15 +8,30 @@ BACKUP_USER_DIR=/backup/${USERNAME}
 BACKUP_DATE_DIR=${BACKUP_USER_DIR}/${DATE}
 BACKUP_COUNT=30
 DATABASE_LIST=/tmp/databases.list
+SITES_LIST=/tmp/sites.list
 WWW_DIR=/var/www
 
 if [[ -n "${USERNAME}" ]];
 then
     mkdir -p ${BACKUP_DATE_DIR}/${USERNAME}/sql
+    mkdir -p ${BACKUP_DATE_DIR}/${USERNAME}/ssl
+    mkdir -p ${BACKUP_DATE_DIR}/${USERNAME}/configs/apache2
+    mkdir -p ${BACKUP_DATE_DIR}/${USERNAME}/configs/nginx
+    mkdir -p ${BACKUP_DATE_DIR}/${USERNAME}/configs/php7.4-fpm
+
     cp -ar ${WWW_DIR}/${USERNAME}/. ${BACKUP_DATE_DIR}/${USERNAME}
+    cp -ar /etc/php/7.4/fpm/pool.d/${USERNAME}.conf ${BACKUP_DATE_DIR}/${USERNAME}/configs/php7.4-fpm/${USERNAME}.conf 2>/dev/null || :
+
+    ls ${WWW_DIR}/${USERNAME} | grep -v php > ${SITES_LIST}
+    for site in `cat ${SITES_LIST}`
+    do
+        cp -ar /etc/letsencrypt/live/${site} ${BACKUP_DATE_DIR}/${USERNAME}/ssl 2>/dev/null || :
+        cp -ar /etc/apache2/sites-enabled/${site}.conf ${BACKUP_DATE_DIR}/${USERNAME}/configs/apache2/${site}.conf 2>/dev/null || :
+        cp -ar /etc/nginx/sites-enabled/${site}.conf ${BACKUP_DATE_DIR}/${USERNAME}/configs/nginx/${site}.conf 2>/dev/null || :
+        cp -ar /etc/php/7.4/fpm/pool.d/${USERNAME}_${site}.conf ${BACKUP_DATE_DIR}/${USERNAME}/configs/php7.4-fpm/${USERNAME}_${site}.conf 2>/dev/null || :
+    done
 
     mysql -u ${DB_USER} -p${DB_PASS} -e "show databases;" | grep "${USERNAME}_*" > ${DATABASE_LIST}
-
     for database in `cat ${DATABASE_LIST}`
     do
         mysqldump -u ${DB_USER} -p${DB_PASS} ${database} > ${BACKUP_DATE_DIR}/${USERNAME}/sql/${database}.sql
@@ -28,6 +43,7 @@ then
     tar -zcf ${DATE}.tar.gz ${USERNAME}
     rm -Rf ${USERNAME}
     rm -f ${DATABASE_LIST}
+    rm -f ${SITES_LIST}
     split -b 50M ${DATE}.tar.gz "${DATE}.tar.gz.part_"
     rm -f ${DATE}.tar.gz
     chown ${USERNAME}:${USERNAME} ${BACKUP_USER_DIR}
