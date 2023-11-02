858afb6dd29c165c5afed65498aa15557f194c9c
EvgenyLavr <lion06@inbox.ru>
2023-11-02 14:40:50 +0300

    fix install php fpm

--- install.sh
+++ install.sh
@@ -75,15 +75,15 @@ cp ./config/etc/mysql/conf.d/my.cnf /etc/mysql/conf.d/my.cnf 2>>"$log"
 systemctl restart mariadb
 
 echo "Install PHP 7.4" | tee -a "$log"
-apt-get install php7.4 php7.4-mysql php7.4-curl php7.4-json php7.4-gd php7.4-zip php7.4-mbstring php7.4-xml php7.4-xmlrpc php7.4-gmp php7.4-memcached php7.4-intl php7.4-bcmath -y 2>>"$log"
+apt-get install php7.4 php7.4-fpm php7.4-mysql php7.4-curl php7.4-json php7.4-gd php7.4-zip php7.4-mbstring php7.4-xml php7.4-xmlrpc php7.4-gmp php7.4-memcached php7.4-intl php7.4-bcmath -y 2>>"$log"
 systemctl restart php7.4-fpm 2>>"$log"
 
 echo "Install PHP 8.0" | tee -a "$log"
-apt-get install php8.0 php8.0-mysql php8.0-curl php8.0-gd php8.0-zip php8.0-mbstring php8.0-xml php8.0-xmlrpc php8.0-gmp php8.0-memcached php8.0-intl php8.0-bcmath -y 2>>"$log"
+apt-get install php8.0 php8.0-fpm php8.0-mysql php8.0-curl php8.0-gd php8.0-zip php8.0-mbstring php8.0-xml php8.0-xmlrpc php8.0-gmp php8.0-memcached php8.0-intl php8.0-bcmath -y 2>>"$log"
 systemctl restart php8.0-fpm 2>>"$log"
 
 echo "Install PHP 8.2" | tee -a "$log"
-apt-get install php8.2 php8.2-mysql php8.2-curl php8.2-gd php8.2-zip php8.2-mbstring php8.2-xml php8.2-xmlrpc php8.2-gmp php8.2-memcached php8.2-intl php8.2-bcmath -y 2>>"$log"
+apt-get install php8.2 php8.2-fpm php8.2-mysql php8.2-curl php8.2-gd php8.2-zip php8.2-mbstring php8.2-xml php8.2-xmlrpc php8.2-gmp php8.2-memcached php8.2-intl php8.2-bcmath -y 2>>"$log"
 systemctl restart php8.2-fpm 2>>"$log"
 
 if [[ "${FORCE_INSTALL}" != "-f" ]]; then
